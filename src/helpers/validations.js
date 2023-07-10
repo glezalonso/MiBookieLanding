2466bb2151eed1e834cffc3d8d8ab45b36c897b7
alonso <glezalonso23@gmail.com>
2023-07-10 13:39:08 -0600

    validations

--- src/helpers/validations.js
+++ src/helpers/validations.js
@@ -1,40 +1,37 @@
 import { toast } from 'react-hot-toast'
 
 export const validateRegister = (values) => {
-  if (!values.fullName) return toast.error('the fullname is required')
-  if (values.fullName.length < 8) return toast.error('the fullname must have at least 8 characters')
-  if (!values.email) return toast.error('the email is required')
-  if (!values.username) return toast.error('the username is required')
-  if (values.username.length < 5) return toast.error('the username must have at least 5 characters')
-  if (values.username.includes(' ')) return toast.error('the username must not have includes spaces ')
-  if (!values.password) return toast.error('the password is required')
-  if (values.password.length < 8) return toast.error('the password mast hace at least 8 characters')
-  if (values.password.includes(' ')) return toast.error(' the password must not have includes spaces')
+  if (!values.fullName) return toast.error('El nombre es requerido')
+  if (values.fullName.length < 8) return toast.error('El nombre debe tener al menos 9 caractéres')
+  if (!values.email) return toast.error('El email es equerido')
+  if (!values.username) return toast.error('El usuario es requerido')
+  if (values.username.length < 5) return toast.error('El usuario debe tener al menos 5 caractéres')
+  if (values.username.includes(' ')) return toast.error('El usuario no debe contener espacios')
+  if (!values.password) return toast.error('La contraseña es requerida')
+  if (values.password.length < 8) return toast.error('La contraseña debe tener al menos 8 caractéres')
+  if (values.password.includes(' ')) return toast.error(' La contraseña no debe contener espacios')
 }
 
 export const validateLogin = (values) => {
-  if (!values.username) return toast.error('the username is required')
-  if (values.username.length < 5) return toast.error('the username must have at least 5 characters')
-  if (values.username.includes(' ')) return toast.error('the username must not have includes spaces ')
-  if (!values.password) return toast.error('the password is required')
-  if (values.password.length < 8) return toast.error('the password mast hace at least 8 characters')
-  if (values.password.includes(' ')) return toast.error(' the password must not have includes spaces')
+  if (!values.username) return toast.error('El usuario es requerido')
+  if (values.username.length < 5) return toast.error('El usuario debe tener al menos 5 caractéres')
+  if (values.username.includes(' ')) return toast.error('El usuario no debe contener espacios ')
+  if (!values.password) return toast.error('La contraseña es requerida')
+  if (values.password.length < 8) return toast.error('La contraseña debe tener al menos 8 caractéres')
+  if (values.password.includes(' ')) return toast.error('  La contraseña no debe contener espacios')
 }
 export const validateEmail = (values) => {
-  if (!values.email) return toast.error('Email is required')
+  if (!values.email) return toast.error('El email es requerido')
 }
 
 export const validateOTP = (values) => {
-  if (!values.OTP) return toast.error('OTP is required')
-  if (values.OTP.length < 5) return toast.error('OTP must have at least 6 characters')
+  if (!values.OTP) return toast.error('EL codigo es requerido')
+  if (values.OTP.length < 5) return toast.error('El codigo debe tener al menos 5 caractéres')
 }
 
 export const validateResetPassword = (values) => {
-  if (!values.password) return toast.error('Password is required')
-  if (!values.confirmPassword) return toast.error('Password is required')
-  if (values.password.length < 8) return toast.error('Password must contain at least 8 characters')
-  if (values.confirmPassword.length < 8) return toast.error('confirm password must contain at least 8 characters')
-  if (values.password.includes(' ')) return toast.error('Password must not include spaces')
-  if (values.confirmPassword.includes(' '))toast.error('Password must not include spaces')
-  if (values.password !== values.confirmPassword) toast.error('Passwods must be equals')
+  if (!values.password) return toast.error('La contraseña es requerida')
+  if (values.password.length < 8) return toast.error('La contraseña debe tener al menos 8 caractéres')
+  if (values.password.includes(' ')) return toast.error('La contraseña no debe contener espacios')
+  if (values.password !== values.confirmPassword) toast.error('Las contraseñas deben de ser iguales')
 }
