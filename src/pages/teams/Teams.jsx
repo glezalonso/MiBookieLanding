f9379a451c397fb9698f34ae93bda0972d8a65d9
alonso <glezalonso23@gmail.com>
2023-07-11 09:57:43 -0600

    with rounded

--- src/pages/teams/Teams.jsx
+++ src/pages/teams/Teams.jsx
@@ -11,7 +11,7 @@ const Teams = () => {
         <Toaster position="top-center" reverseOrder={false} />
         <Container fluid >
         <Row className='my-2 mx-auto'>
-            <Col md={12} className='bg-dark text-white border rounded mx-auto fs-4'>
+            <Col md={12} className='bg-dark text-white  rounded mx-auto fs-4'>
             <SectionTeams />
             </Col>
         </Row>
