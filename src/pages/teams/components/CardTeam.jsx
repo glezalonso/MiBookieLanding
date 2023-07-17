202b5b20d78dcd8bb7564c0e2b9b8c8013797993
alonso <glezalonso23@gmail.com>
2023-07-17 11:50:53 -0600

    Card team

--- src/pages/teams/components/CardTeam.jsx
+++ src/pages/teams/components/CardTeam.jsx
@@ -5,23 +5,29 @@ const CardTeam = ({ team }) => {
     return (
         <>
             <section>
-                <Card className="bg-dark text-light">
-                    <Card.Header>
-                        <Card.Img
-                            src={team?.poster}
-                            alt={`image ${team?.name}`}
-                        />
-                    </Card.Header>
-                    <Card.Body>
-                        <Card.Title>{team?.name}</Card.Title>
-                        <Card.Text className="my-1">
-                            Estadio: {team?.stadium}
-                        </Card.Text>
-                        <Card.Text className="my-1">
-                            Deporte: {team?.sport?.sport}
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
+                <div className="d-flex justify-content-center">
+                    <Card className="bg-dark text-light">
+                        <Card.Header>
+                            <Card.Img
+                                style={{
+                                    height: '200px',
+                                    width: '200px',
+                                }}
+                                src={team?.poster}
+                                alt={`image ${team?.name}`}
+                            />
+                        </Card.Header>
+                        <Card.Body>
+                            <Card.Title>{team?.name}</Card.Title>
+                            <Card.Text className="my-1">
+                                Estadio: {team?.stadium}
+                            </Card.Text>
+                            <Card.Text className="my-1">
+                                Deporte: {team?.sport?.sport}
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
             </section>
         </>
     )
