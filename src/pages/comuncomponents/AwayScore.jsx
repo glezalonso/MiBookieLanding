6ef75bba62f78dcd19c2f787449ab474e09a55f4
alonso <glezalonso23@gmail.com>
2023-08-08 20:13:42 -0600

    score

--- src/pages/comuncomponents/AwayScore.jsx
+++ src/pages/comuncomponents/AwayScore.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AwayScore = ({ match }) => {
     return (
         <>
-            <div className="my-1">
+            <div className="my-1 text-xs font-bold">
                 <strong>{match?.score?.map((score) => score?.away)}</strong>
             </div>
         </>
--- src/pages/comuncomponents/LocalScore.jsx
+++ src/pages/comuncomponents/LocalScore.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const LocalScore = ({ match }) => {
     return (
         <>
-            <div className="my-1">
+            <div className="my-1 text-xs font-bold">
                 <strong>{match?.score?.map((score) => score?.local)}</strong>
             </div>
         </>
