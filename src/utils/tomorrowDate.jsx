8f2cd3248d94d7c914fa7fdd1511943808cdf427
Arturo Alonso Gonzalez Serrano <135847698+glezalonso@users.noreply.github.com>
2023-08-09 08:19:00 -0600

    Actualizar tomorrowDate.jsx

--- src/utils/tomorrowDate.jsx
+++ src/utils/tomorrowDate.jsx
@@ -3,15 +3,15 @@ export default (tomorrowDate) => {
     const year = new Date().getFullYear()
     const month = new Date().getMonth()
     if (month < 10) {
-        if (day < 10) {
+        if (day+1 < 10) {
             const date = `${year}-0${month + 1}-0${day + 1}`
             return date
         } else if (day + 1 > 31) {
             const date = `${year}-0${month + 2}-${'01'}`
             return date
         } else {
-                const date = `${year}-0${month + 1}-${day}`
-                return date
+                const date = `${year}-0${month + 1}-${day+1}`
+                    return date
         }
     } else {
         if (day + 1 < 10) {
