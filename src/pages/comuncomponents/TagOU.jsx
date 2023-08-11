6c791801147f0b8dcc09572d9e5f012baa2e5443
alonso <glezalonso23@gmail.com>
2023-08-11 04:57:29 -0600

    Odd Over Under

--- src/pages/comuncomponents/TagOU.jsx
+++ src/pages/comuncomponents/TagOU.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const TagOU = ({ match }) => {
     return (
         <>
-            <div className="p-1  w-2/4 mx-auto flex justify-between rounded-md border-2 border-gray-200 text-sm text-center text-gray-500">
+            <div className="flex justify-between mb-1 w-2/4 mx-auto p-1 rounded-md border-2 border-gray-200 text-sm">
                 <div>
-                    <span className="text-sm text-gray-500">
-                        {match?.oddOverUnder?.split(' ', 1)}
+                    <span className="justify-start-start text-sm text-gray-500">
+                        Total:{match?.oddOverUnder?.split(' ', 1)}
                     </span>
                 </div>
                 <div>
-                    <span className="flex  gap-2   text-sm text-black font-semibold">
+                    <span className="justify-end flex gap-2   text-sm text-black font-semibold">
                         <div>{match?.oddOverUnder?.split(' ')[1]}</div>
                         <div>{match?.oddOverUnder?.split(' ')[2]}</div>
                     </span>
