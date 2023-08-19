2575ffdf5c82a5cdd918a3720538b8451e80f753
alonso <glezalonso23@gmail.com>
2023-08-19 11:53:02 -0600

    filter

--- src/pages/home/components/SectionMatches.jsx
+++ src/pages/home/components/SectionMatches.jsx
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import { useGetMatchesToday } from '../../../features/matches.features'
-import { Select, Alert, Badge, TextInput } from 'flowbite-react'
-import { useGetSports } from '../../../features/sports.features'
+import { Alert, Badge, TextInput } from 'flowbite-react'
 import { toast } from 'react-hot-toast'
 import CardMatch from '../../comuncomponents/CardMatch'
 import Loading from '../../../ui/Loading'
-import filters from '../../../icons/filter.svg'
 import matchIcon from '../../../icons/match.svg'
 import { useInView } from 'react-intersection-observer'
 
@@ -23,83 +21,56 @@ const SectionMatches = ({ date }) => {
     const matches =
         data?.pages?.reduce((prev, pages) => prev.concat(pages.data), []) ?? []
 
-    const { data: sports } = useGetSports()
-
-    const [search, setSearch] = useState(false)
     const [filter, setFilter] = useState('')
 
-    matches?.sort((a, b) => b.status - a.status)
     if (isLoading) return <Loading />
     if (isError) return toast.error('Hubo un error al cargar los partidos!')
 
     const matchFilter = matches?.filter((match) => {
         if (!filter) return match
-        return (
-            match?.local?.name?.toLowerCase().includes(filter.toLowerCase()) ||
-            match?.away?.name?.toLowerCase().includes(filter.toLowerCase()) ||
-            match?.sport?._id?.includes(filter.toLowerCase())
-        )
+        return match?.sport?.sport?.toLowerCase().includes(filter.toLowerCase())
     })
 
     return (
         <>
-            <section className="  min-h-screen">
-                <div className="flex justify-between  mx-2 ">
-                    <h5 className=" flex mt-3">
-                        <div className="flex mt-1 mb-2">
+            <section className="min-h-fit">
+                <div className="w-full flex justify-between items-center mt-10  ">
+                    <div className="flex justify-between items-center">
+                        <div className=" mx-2 flex gap-1">
                             <img
                                 src={matchIcon}
                                 alt="partidos"
-                                className="w-7 h-7 mb- ml-1"
+                                className=" block w-7 h-7 mb- ml-1"
                             />
                             <span className="block mx-1 mt-1 ">Partidos</span>
                         </div>
-                        <Badge
-                            size={'sm'}
-                            className=" mt-1.5 bg-zinc-900 text-gray-200 p-1"
-                        >
-                            {data?.pages[0]?.total}
-                        </Badge>
-                    </h5>
-                    <div
-                        onClick={() => setSearch(!search)}
-                        className="flex  mt-6  mr-2 gap-1 hover:cursor-pointer"
-                    >
-                        Filtro
-                        <img src={filters} alt="Filtro" className="h-5 w-5" />
+                        <div>
+                            <Badge
+                                size={'sm'}
+                                className=" mt-1.5 bg-zinc-900 text-gray-200 p-1"
+                            >
+                                {data?.pages[0]?.total}
+                            </Badge>
+                        </div>
                     </div>
-                </div>
-                {search ? (
-                    <div className="w-full flex justify-between gap-2 my-1 mx-auto p-1  ">
+                    <div className="self-end">
                         <TextInput
                             sizing={'md'}
-                            className="w-3/5 text-base focus:text-base active:text-base "
-                            name="team"
-                            placeholder="Equipo..."
+                            className="text-base focus:text-base active:text-base "
+                            name="sport"
+                            placeholder="Deporte"
                             onChange={(e) => setFilter(e.target.value)}
                         />
-
-                        <Select
-                            className="rounded text-base w-2/5 mx-auto "
-                            onChange={(e) => setFilter(e.target.value)}
-                        >
-                            <option value="6">Todos</option>
-
-                            {sports?.map((sport) => (
-                                <option key={sport?._id} value={sport?._id}>
-                                    {sport?.sport}
-                                </option>
-                            ))}
-                        </Select>
                     </div>
-                ) : null}
-
+                </div>
                 {matchFilter?.length > 0 ? (
                     matchFilter?.map((match) => (
                         <CardMatch key={match?._id} match={match} />
                     ))
                 ) : (
-                    <Alert></Alert>
+                    <Alert color={'warning'}>
+                        No hay partidos disponibles!
+                    </Alert>
                 )}
             </section>
             <div ref={ref}></div>
