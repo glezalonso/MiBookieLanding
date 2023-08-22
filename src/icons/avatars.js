a7805c891d6f29b6b4447d521c80ab1227170bc6
alonso <glezalonso23@gmail.com>
2023-08-22 12:39:12 -0600

    avatares

--- src/icons/avatars.js
+++ src/icons/avatars.js
@@ -29,4 +29,34 @@ export const avatars = [
         name: 'avatar 6',
         url: 'https://res.cloudinary.com/dei46qlds/image/upload/v1692205082/avatar_1_unlbr1.svg',
     },
+    {
+        id: 7,
+        name: 'avatar 7',
+        url: 'https://res.cloudinary.com/dei46qlds/image/upload/v1692729127/avatar_1_qavjti.svg',
+    },
+    {
+        id: 8,
+        name: 'avatar 8',
+        url: 'https://res.cloudinary.com/dei46qlds/image/upload/v1692729127/avatar_2_szt9tg.svg',
+    },
+    {
+        id: 9,
+        name: 'avatar 9',
+        url: 'https://res.cloudinary.com/dei46qlds/image/upload/v1692729127/avatar_xvfyom.svg',
+    },
+    {
+        id: 10,
+        name: 'avatar 10',
+        url: 'https://res.cloudinary.com/dei46qlds/image/upload/v1692729297/avatar_3_gtknnr.svg',
+    },
+    {
+        id: 11,
+        name: 'avatar 11',
+        url: 'https://res.cloudinary.com/dei46qlds/image/upload/v1692729368/avatar_4_woknza.svg',
+    },
+    {
+        id: 12,
+        name: 'avatar 12',
+        url: 'https://res.cloudinary.com/dei46qlds/image/upload/v1692729405/avatar_5_pfgzcg.svg',
+    },
 ]
