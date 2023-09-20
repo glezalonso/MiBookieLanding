7e1d6e1a30280d83064588fcfebae2edfe8a0c63
alonso <glezalonso23@gmail.com>
2023-09-20 10:48:36 -0600

    new

--- src/pages/news/components/CardNew.jsx
+++ src/pages/news/components/CardNew.jsx
@@ -7,22 +7,22 @@ const CardNew = ({ content }) => {
     const [seeMore, setSeeMore] = useState(false)
     return (
         <>
-            <section className="bg-white rounded p-1 my-3 lg:hover:shadow-xl  ">
-                <Table hoverable className="table-auto mt-1 text-sm">
+            <section className=" w-full bg-white rounded p-1 my-3 shadow-xl  ">
+                <Table hoverable className=" mt-1 text-sm">
                     <Table.Body>
                         <Table.Row>
-                            <Table.Cell className="items-center text-center">
+                            <Table.Cell className="items-center justify-start text-center">
                                 <img
                                     src={content?.league?.poster}
-                                    className="w-14 h-w-14"
+                                    className="w-12 h-12"
                                 />
 
-                                <p className=" w-14 mt-2 text-gray-600 font-bold text-xs">
+                                <p className=" w-12 mt-2 text-gray-600 font-bold text-xs">
                                     {content?.league?.league}
                                 </p>
                             </Table.Cell>
-                            <Table.Cell>
-                                <h5 className="font-bold text-gray-800 mb-1 text-base ">
+                            <Table.Cell className="px-0">
+                                <h5 className="font-bold text-gray-800 mb-1 text-base break-word">
                                     {content?.title}
                                 </h5>
                                 <p
@@ -34,7 +34,7 @@ const CardNew = ({ content }) => {
                                 >
                                     {content?.content}
                                 </p>
-                                <div className="flex mt-2.5 justify-end items-center  text-gray-800">
+                                <div className="flex  flex-wrap mt-2.5 justify-start items-center w-max  text-gray-800">
                                     <Link
                                         className=" text-blue-500 mx-1 hover:underline mr-2 border-2 border-blue-500 rounded-lg px-2 py-1"
                                         onClick={() => setSeeMore(!seeMore)}
