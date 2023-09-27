ff509a86cd394b6edb72826c7db2b7b62e1ca2a7
alonso <glezalonso23@gmail.com>
2023-09-27 07:35:53 -0600

    changes

--- src/utils/tomorrowDate.jsx
+++ src/utils/tomorrowDate.jsx
@@ -6,7 +6,7 @@ export default (tomorrowDate) => {
         if (day + 1 < 10) {
             const date = `${year}-0${month + 1}-0${day + 1}`
             return date
-        } else if (day + 1 > 31) {
+        } else if (day + 1 > 30) {
             const date = `${year}-0${month + 2}-${'01'}`
             return date
         } else {
@@ -17,7 +17,7 @@ export default (tomorrowDate) => {
         if (day + 1 < 10) {
             const date = `${year}-${month + 1}-0${day + 1}`
             return date
-        } else if (day + 1 > 31) {
+        } else if (day + 1 > 30) {
             const date = `${year}-${month + 2}-${'01'}`
             return date
         } else {
